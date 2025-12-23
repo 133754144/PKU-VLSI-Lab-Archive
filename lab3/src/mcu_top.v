9cac8a00ca8f9fe8eb22d3339804923196e42ec6
133754144 <79274062+133754144@users.noreply.github.com>
2025-12-23 15:49:58 +0800

    Update mcu_top.v
    maybe it works

--- lab3/src/mcu_top.v
+++ lab3/src/mcu_top.v
@@ -34,9 +34,44 @@ module mcu_top(
     );
     // 实例化 ALU 运算单元
     wire [7:0] alu_out;
-        alu alu_inst (
-            .in1  (in1_reg),
-            .in2  (in2_reg),
-            .sel  (sel_reg),
-            .out1 (alu_out)
-        );
+    alu alu_inst (
+        .in1  (in1_reg),
+        .in2  (in2_reg),
+        .sel  (sel_reg),
+        .out1 (alu_out)
+    );
+    // 输入寄存器：在控制信号 input_ld 有效时锁存 ALU 操作数和操作码
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin
+            in1_reg <= 8'd0;
+            in2_reg <= 8'd0;
+            sel_reg <= 3'd0;
+        end else if (input_ld) begin //若input_ID无效，寄存器保持原数据
+            in1_reg <= in1;
+            in2_reg <= in2;
+            sel_reg <= sel;
+        end
+    end
+    // 结果寄存器：在控制信号 result_ld 有效时锁存 ALU 计算结果
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin
+            result_reg <= 8'd0;
+        end else if (result_ld) begin
+            result_reg <= alu_out;
+        end
+    end
+    // 组装RAM写入的数据（16位）：高8位填0，低8位为ALU结果
+    assign mem_din = {8'h00, result_reg};
+     // 实例化 RAM 存储单元
+    ram_256x16 u_ram (
+        .clk  (clk),
+        .CEB  (mem_CEB),
+        .WEB  (mem_WEB),
+        .RSTB (reset),    // 复位信号直接连接（低有效）
+        .Addr (mem_addr),
+        .Data (mem_din),
+        .Q    (mem_q)
+    );
+    // 将RAM的输出直接连接到顶层输出端口
+    assign out_data = mem_q;
+endmodule
